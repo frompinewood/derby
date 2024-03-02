a83a4abb6116c808498129bc6348fbb5d95ae995
Micaiah Parker <frompinewood@icloud.com>
2024-03-01 21:59:18 -0500

    add eunit,add str pattern

--- src/derby.erl
+++ src/derby.erl
@@ -1,7 +1,7 @@
 -module(derby).
 -export([parse/1, possible/1, query/1, roll/1, chance/2]).
 
--type roll()  :: {roll, list(integer()), integer(), list(modifier())}.
+-type roll()  :: {roll, list(integer()), integer(), list(modifier())} | string().
 -type modifier() :: high 
                   | low 
                   | plus 
@@ -20,6 +20,8 @@ query(Query) ->
     roll(parse(Query)).
 
 -spec roll(roll()) -> result().
+roll(Roll) when is_list(Roll) ->
+    roll(parse(Roll));
 roll({roll, Dice, Bonus, Mods}) ->
     DiceResult = lists:map(fun rand:uniform/1, Dice),
     ModdedResult = lists:foldl(fun modify/2, DiceResult, Mods),
@@ -35,6 +37,8 @@ modify({Mod, ModVal}, Result) ->
     end.
 
 -spec possible(roll()) -> list(integer()).
+possible(Roll) when is_list(Roll) ->
+    possible(parse(Roll));
 possible({roll, Dice, Bonus, Mods}) ->
     Possible = possible(Dice),
     lists:map(fun (L) -> lists:sum(L) + Bonus end,
@@ -46,6 +50,8 @@ possible(Acc, [H|T]) ->
     possible([X++[Y] || X <- Acc, Y <- lists:seq(1, H)], T).
 
 -spec chance(roll(), integer()) -> float().
+chance(Roll, Target) when is_list(Roll) ->
+    chance(parse(Roll), Target);
 chance(Roll, Target) ->
     P = possible(Roll),
     S = lists:filter(fun (X) -> X >= Target end, P),
